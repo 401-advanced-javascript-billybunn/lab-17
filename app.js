4c5a8ea0e5da11a8b28603409978c2411ad99417
Billy Bunn <williammbunn@gmail.com>
2019-04-09 15:28:16 -0700

    makes it work with promises and separate functions

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 'use strict';
 
 const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 const events = require('./event-pool.js');
 require('./logger.js');
 
 
-const alterFile = (file) => {
-  try {
-    fs.readFile(file, (err, data) => {
-      let text = data.toString().toUpperCase();
-      fs.writeFile(file, Buffer.from(text), (err, data) => {
-        events.emit('file-save', {status: 1, file: file, text: 'Saved Properly'});
-      });
-    });
-  }
-  catch (e) {
-    console.log('something something');
-    events.emit('file-error', {status:0, file: file, text: e.message});
-  }
+const read = (file) => readFile(file);
+const write = (file, buffer) => writeFile(file, buffer);
+const uppercase = (buffer) => Buffer.from(buffer.toString().trim().toUpperCase());
 
+const alterFile = (file) => {
+  read(file)
+    .then(buffer => uppercase(buffer))
+    .then(buffer => write(file, buffer))
+    .then(success => events.emit('file-save', { status: 1, file: file, text: 'Saved Properly' }))
+    .catch(error => events.emit('file-error', { status: 0, file: file, text: error.message }));
 };
 
 let file = process.argv.slice(2).shift();
